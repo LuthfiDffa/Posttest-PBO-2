f90ddadfe2792a535411039d12be088a0c6fa4bf
LuthfiDffa <lutpiixd@gmail.com>
2025-09-15 22:27:57 +0800

    Add files via upload

---
+++ mobilsport.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+public class mobilsport {  
+    private String merk;                   
+    private String model;
+    private int tahun;
+    private String harga;
+    
+    public mobilsport(String merk, String model, int tahun, String harga) {
+        this.merk = merk;
+        this.model = model;
+        this.tahun = tahun;
+        this.harga = harga;
+}
+
+public String getMerk() { return merk; }
+    public String getModel() { return model; }
+    public int getTahun() { return tahun; }
+    public String getHarga() { return harga; }
+}
